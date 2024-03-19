0e3766b1c3025b33a99f02a1379e76105facc119
bmarkondareddy <bmarkondareddy@yahoo.com>
2024-03-19 07:46:43 +0530

    updated file

--- app.java
+++ app.java
@@ -2,5 +2,13 @@ public class App{
 	System.out.print("Welcome to java...");
 	System.out.print("for fetch ");
         System.out.print("Welcome to GUI");
+System.out.print("Welcome to GUI");
+
+System.out.print("Welcome to GUI1");
+
+System.out.print("Welcome to GUI2");
+
+System.out.print("Welcome to GUI2");
+
 
 }
